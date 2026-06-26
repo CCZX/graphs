314f0ee5dd878a2d185d4fdc38853c1320d6accd
chenchao <ccdebuging@gmail.com>
2026-06-27 00:47:29 +0800

    feat: di

---
+++ src/common/context.tsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, ReactNode } from 'react';
+import { Container } from 'inversify';
+import { container } from './container';
+
+const DIContext = createContext<Container | null>(null);
+
+export const ContextProvider = ({ children }: { children: ReactNode }) => {
+	return <DIContext.Provider value={container}>{children}</DIContext.Provider>;
+};
+
+export function useInject<T>(token: symbol): T {
+	const container = useContext(DIContext);
+	if (!container) {
+		throw new Error('useInject must be used within a ContextProvider');
+	}
+	return container.get<T>(token);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'reflect-metadata';
 import './normalized.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
